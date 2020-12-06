b4c9863fc4eb5ac0f7bb5f69b3a0078e1af871de
duyixian <duyixian1234@qq.com>
2020-12-06 19:27:49 +0800

    add `basic_config` function

--- README.md
+++ README.md
@@ -9,7 +9,10 @@ pip install -U redis-rate-limiter
 ## Use
 
 ```python
+from redis_rate_limiter.config imoport basic_config
 from redis_rate_limiter.rate_limiter import RateLimiter
+
+basic_config(redis_url='redis://localhost:6379/0')
 
 @RateLimiter(10, period=1)
 def greet():
--- redis_rate_limiter/config.py
+++ redis_rate_limiter/config.py
@@ -1,12 +1,18 @@
-from dataclasses import dataclass, field
 import socket
-import os
+from dataclasses import dataclass, field
 
 
 @dataclass
 class Settings:
-    redis_url: str = os.getenv("REDIS_RATE_LIMITER_DSN", "redis://localhost:6379/0")
+    redis_url: str = ""
     key_prefix: str = field(default_factory=socket.gethostname)
 
 
 settings = Settings()
+
+
+def basic_config(redis_url: str = "", key_prefix: str = ""):
+    if redis_url:
+        settings.redis_url = redis_url
+    if key_prefix:
+        settings.key_prefix = key_prefix
